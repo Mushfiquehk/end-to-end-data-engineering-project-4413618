43757daf4120457279a3f32ef7111b75cc039c7f
Mushfiquehk <mushfiquehasankhan@gmail.com>
2024-01-21 13:22:33 -0600

    added cron job for bsj

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,7 +1,12 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, AssetSelection, ScheduleDefinition, define_asset_job
 
-from . import assets
+from .assets import resources
 
-all_assets = load_assets_from_modules([assets])
+big_star_jobs = define_asset_job("big-star-job", selection=AssetSelection.all())
+big_star_schedules = ScheduleDefinition(
+    job=big_star_jobs,
+    cron_schedule="0 * * * *",
+)
 
-defs = Definitions(assets=load_assets_from_modules([assets], resources=assets.resources))
+defs = Definitions(assets=load_assets_from_modules([assets], resources=resources,
+                                                   jobs=[big_star_jobs], schedules=[big_star_schedules]))
